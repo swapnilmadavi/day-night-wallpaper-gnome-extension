eba31733767c5773ac1e3d50aa089118a0ae8065
Swapnil Madavi <swapy.madavi04@gmail.com>
2021-04-27 23:25:13 +0530

    Refine v2 Prefs UI

--- prefs.js
+++ prefs.js
@@ -10,80 +10,193 @@ const SettingsUi = Me.imports.settingsUi;
 const Utils = Me.imports.utils;
 
 let DayNightWallpaperPrefsWidget = GObject.registerClass(
-    class DayNightWallpaperPrefsWidget extends Gtk.Box {
-        _init(settings) {
-            super._init({
-                margin: 18,
-                spacing: 12,
-                orientation: Gtk.Orientation.VERTICAL
-            })
-    
-            this._settings = settings;
+class DayNightWallpaperPrefsWidget extends Gtk.Grid {
+    _init(settings) {
+        super._init({
+            margin: 18,
+            column_spacing: 12,
+            row_spacing: 12,
+        })
 
-            const _pictureOptions = Utils.getAvailablePictureOptions();
-    
-            // ** Day Wallpaper Settings section **
-            this._dayWallpaperSettingsSection = new SettingsUi.WallpaperSettingsSection('Day Wallpaper', _pictureOptions);
-    
-            this._dayWallpaperSettingsSection.setWallpaperUri(settings.get_string('day-wallpaper'));
-            this._dayWallpaperSettingsSection.setWallpaperAdjustment(settings.get_string('day-wallpaper-adjustment'));
-            const _dayWallpaperSwitchTime = Utils.SwitchTime.newFromSettings(settings.get_double('day-wallpaper-switch-time'));
-            this._dayWallpaperSettingsSection.setWallpaperSwitchTime(_dayWallpaperSwitchTime.switchHour, _dayWallpaperSwitchTime.switchMinute);
-    
-            this._dayWallpaperSettingsSection.imageChooserButton.connect('file-set', () => {
-                const wallpaperUri = this._dayWallpaperSettingsSection.getWallpaperUri()
-                this._settings.set_string('day-wallpaper', wallpaperUri);
-            });
-            this._dayWallpaperSettingsSection.adjustmentComboBoxText.connect('changed', () => {
-                const wallpaperAdjustment = this._dayWallpaperSettingsSection.getWallpaperAdjustment()
-                this._settings.set_string('day-wallpaper-adjustment', wallpaperAdjustment);
-            });
-            this._dayWallpaperSettingsSection.switchTimeWidget.hourSpinButton.connect('value-changed', this._onDayWallpaperSwitchTimeChanged.bind(this));
-            this._dayWallpaperSettingsSection.switchTimeWidget.minuteSpinButton.connect('value-changed', this._onDayWallpaperSwitchTimeChanged.bind(this));
+        // Day-Night Wallpaper settings
+        this._settings = settings;
 
-            this.pack_start(this._dayWallpaperSettingsSection, false, true, 0);
+        // Image adjustment options
+        this._pictureOptions = Utils.getAvailablePictureOptions().sort();
 
-            // ** Night Wallpaper Settings section **
-            this._nightWallpaperSettingsSection = new SettingsUi.WallpaperSettingsSection('Night Wallpaper', _pictureOptions);
-    
-            this._nightWallpaperSettingsSection.setWallpaperUri(settings.get_string('night-wallpaper'));
-            this._nightWallpaperSettingsSection.setWallpaperAdjustment(settings.get_string('night-wallpaper-adjustment'));
-            const _nightWallpaperSwitchTime = Utils.SwitchTime.newFromSettings(settings.get_double('night-wallpaper-switch-time'));
-            this._nightWallpaperSettingsSection.setWallpaperSwitchTime(_nightWallpaperSwitchTime.switchHour, _nightWallpaperSwitchTime.switchMinute);
-    
-            this._nightWallpaperSettingsSection.imageChooserButton.connect('file-set', () => {
-                const wallpaperUri = this._nightWallpaperSettingsSection.getWallpaperUri()
-                this._settings.set_string('night-wallpaper', wallpaperUri);
-            });
-            this._nightWallpaperSettingsSection.adjustmentComboBoxText.connect('changed', () => {
-                const wallpaperAdjustment = this._nightWallpaperSettingsSection.getWallpaperAdjustment()
-                this._settings.set_string('night-wallpaper-adjustment', wallpaperAdjustment);
-            });
-            this._nightWallpaperSettingsSection.switchTimeWidget.hourSpinButton.connect('value-changed', this._onNightWallpaperSwitchTimeChanged.bind(this));
-            this._nightWallpaperSettingsSection.switchTimeWidget.minuteSpinButton.connect('value-changed', this._onNightWallpaperSwitchTimeChanged.bind(this));
+        // Image files filter
+        this._imageFileFilter = new Gtk.FileFilter();
+        this._imageFileFilter.set_name('Image files');
+        this._imageFileFilter.add_mime_type('image/jpg');
+        this._imageFileFilter.add_mime_type('image/png');
+        this._imageFileFilter.add_mime_type('image/jpeg');
+        this._imageFileFilter.add_mime_type('image/bmp');
 
-            this.pack_start(this._nightWallpaperSettingsSection, false, true, 0);
-    
-            // ** About section **
-            const _aboutSection = new SettingsUi.AboutSection();
-            _aboutSection.set_margin_top(30);
-            this.pack_start(_aboutSection, false, true, 0);
+        this._dayWallpaperImageChooserButton = this._createWallpaperImageChooserButton(settings.get_string('day-wallpaper'));
+        this._dayWallpaperAdjustmentComboBoxText = this._createWallpaperAdjustmentComboBoxText(settings.get_string('day-wallpaper-adjustment'));
+        this._dayWallpaperSwitchTimeWidget = new SettingsUi.SwitchTimeWidget();
+        const _dayWallpaperSwitchTime = Utils.SwitchTime.newFromSettings(settings.get_double('day-wallpaper-switch-time'));
+        this._dayWallpaperSwitchTimeWidget.setSwitchTime(_dayWallpaperSwitchTime.switchHour, _dayWallpaperSwitchTime.switchMinute);
+
+        this._nightWallpaperImageChooserButton = this._createWallpaperImageChooserButton(settings.get_string('night-wallpaper'));
+        this._nightWallpaperAdjustmentComboBoxText = this._createWallpaperAdjustmentComboBoxText(settings.get_string('night-wallpaper-adjustment'));
+        this._nightWallpaperSwitchTimeWidget = new SettingsUi.SwitchTimeWidget();
+        const _nightWallpaperSwitchTime = Utils.SwitchTime.newFromSettings(settings.get_double('night-wallpaper-switch-time'));
+        this._nightWallpaperSwitchTimeWidget.setSwitchTime(_nightWallpaperSwitchTime.switchHour, _nightWallpaperSwitchTime.switchMinute);
+
+        this._buildDayWallpaperSection();
+        this._buildNightWallpaperSection();
+        this._buildAboutSection();
+
+        this._connectSettings();
+    }
+
+    _createWallpaperImageChooserButton(imageUri) {
+        let wallpaperImageChooserButton = new Gtk.FileChooserButton({
+            title: 'Image File',
+            action: Gtk.FileChooserAction.OPEN,
+            halign: Gtk.Align.END,
+            width_chars: 40,
+            filter: this._imageFileFilter
+        });
+        wallpaperImageChooserButton.set_uri(imageUri);
+        return wallpaperImageChooserButton;
+    }
+
+    _createWallpaperAdjustmentComboBoxText(adjustmentOption) {
+        let wallpaperAdjustmentComboBoxText = new Gtk.ComboBoxText();
+        let adjustmentOptionIndex = 0;
+        for (let i = 0; i < this._pictureOptions.length; i++) {
+            if (this._pictureOptions[i] == adjustmentOption) {
+                adjustmentOptionIndex = i;
+            }
+            wallpaperAdjustmentComboBoxText.append_text(this._capitalise(this._pictureOptions[i]));
         }
+        wallpaperAdjustmentComboBoxText.set_active(adjustmentOptionIndex);
+        return wallpaperAdjustmentComboBoxText;
+    }
 
-        _onDayWallpaperSwitchTimeChanged(spinButton) {
-            const daySwitchHour = this._dayWallpaperSettingsSection.switchTimeWidget.hourSpinButton.get_value_as_int();
-            const daySwitchMinute = this._dayWallpaperSettingsSection.switchTimeWidget.minuteSpinButton.get_value_as_int();
-            const daySwitchTime = new Utils.SwitchTime(daySwitchHour, daySwitchMinute);
-            this._settings.set_double('day-wallpaper-switch-time', daySwitchTime.toSettingsFormat());
-        }
-    
-        _onNightWallpaperSwitchTimeChanged(spinButton) {
-            const nightSwitchHour = this._nightWallpaperSettingsSection.switchTimeWidget.hourSpinButton.get_value_as_int();
-            const nightSwitchMinute = this._nightWallpaperSettingsSection.switchTimeWidget.minuteSpinButton.get_value_as_int();
-            const nightSwitchTime = new Utils.SwitchTime(nightSwitchHour, nightSwitchMinute);
-            this._settings.set_double('night-wallpaper-switch-time', nightSwitchTime.toSettingsFormat());
-        }
-    });
+    _buildDayWallpaperSection() {
+        // Day wallpaper title
+        const _dayWallpaperTitleLabel = new Gtk.Label({
+            label: `<b>Day Wallpaper</b>`,
+            halign: Gtk.Align.START,
+            use_markup: true
+        });
+        this.attach(_dayWallpaperTitleLabel, 0, 1, 2, 1);
+
+        // Day wallpaper image
+        const _dayWallpaperImageLabel = new Gtk.Label({
+            label: 'Image',
+            halign: Gtk.Align.START,
+            hexpand: true
+        });
+        this.attach(_dayWallpaperImageLabel, 0, 2, 1, 1);
+        this.attach_next_to(this._dayWallpaperImageChooserButton, _dayWallpaperImageLabel, Gtk.PositionType.RIGHT, 1, 1);
+
+        // Day wallpaper adjustment
+        const _dayWallpaperAdjustmentLabel = new Gtk.Label({
+            label: 'Adjustment',
+            halign: Gtk.Align.START,
+            hexpand: true
+        });
+        this.attach(_dayWallpaperAdjustmentLabel, 0, 3, 1, 1);
+        this.attach_next_to(this._dayWallpaperAdjustmentComboBoxText, _dayWallpaperAdjustmentLabel, Gtk.PositionType.RIGHT, 1, 1);
+
+        // Day wallpaper switch time
+        const _dayWallpaperSwitchTimeLabel = new Gtk.Label({
+            label: 'Switch Time (24-hour)',
+            halign: Gtk.Align.START,
+            hexpand: true
+        });
+        this.attach(_dayWallpaperSwitchTimeLabel, 0, 4, 1, 1);
+        this.attach_next_to(this._dayWallpaperSwitchTimeWidget, _dayWallpaperSwitchTimeLabel, Gtk.PositionType.RIGHT, 1, 1);
+    }
+
+    _buildNightWallpaperSection() {
+        // Night wallpaper title
+        const _nightWallpaperTitleLabel = new Gtk.Label({
+            label: `<b>Night Wallpaper</b>`,
+            halign: Gtk.Align.START,
+            use_markup: true,
+            margin_top: 18
+        });
+        this.attach(_nightWallpaperTitleLabel, 0, 5, 2, 1)
+
+        // Night wallpaper image
+        const _nightWallpaperImageLabel = new Gtk.Label({
+            label: 'Image',
+            halign: Gtk.Align.START,
+            hexpand: true
+        });
+        this.attach(_nightWallpaperImageLabel, 0, 6, 1, 1);
+        this.attach_next_to(this._nightWallpaperImageChooserButton, _nightWallpaperImageLabel, Gtk.PositionType.RIGHT, 1, 1);
+
+        // Night wallpaper adjustment
+        const _nightWallpaperAdjustmentLabel = new Gtk.Label({
+            label: 'Adjustment',
+            halign: Gtk.Align.START,
+            hexpand: true
+        });
+        this.attach(_nightWallpaperAdjustmentLabel, 0, 7, 1, 1);
+        this.attach_next_to(this._nightWallpaperAdjustmentComboBoxText, _nightWallpaperAdjustmentLabel, Gtk.PositionType.RIGHT, 1, 1);
+
+        // Night wallpaper switch time
+        const _nightWallpaperSwitchTimeLabel = new Gtk.Label({
+            label: 'Switch Time (24-hour)',
+            halign: Gtk.Align.START,
+            hexpand: true
+        });
+        this.attach(_nightWallpaperSwitchTimeLabel, 0, 8, 1, 1);
+        this.attach_next_to(this._nightWallpaperSwitchTimeWidget, _nightWallpaperSwitchTimeLabel, Gtk.PositionType.RIGHT, 1, 1);
+    }
+
+    _buildAboutSection() {
+        const _aboutSection = new SettingsUi.AboutSection();
+        _aboutSection.set_margin_top(18);
+        this.attach(_aboutSection, 0, 9, 2, 1);
+    }
+
+    _onDayWallpaperSwitchTimeChanged(spinButton) {
+        const daySwitchHour = this._dayWallpaperSwitchTimeWidget.hourSpinButton.get_value_as_int();
+        const daySwitchMinute = this._dayWallpaperSwitchTimeWidget.minuteSpinButton.get_value_as_int();
+        const daySwitchTime = new Utils.SwitchTime(daySwitchHour, daySwitchMinute);
+        this._settings.set_double('day-wallpaper-switch-time', daySwitchTime.toSettingsFormat());
+    }
+
+    _onNightWallpaperSwitchTimeChanged(spinButton) {
+        const nightSwitchHour = this._nightWallpaperSwitchTimeWidget.hourSpinButton.get_value_as_int();
+        const nightSwitchMinute = this._nightWallpaperSwitchTimeWidget.minuteSpinButton.get_value_as_int();
+        const nightSwitchTime = new Utils.SwitchTime(nightSwitchHour, nightSwitchMinute);
+        this._settings.set_double('night-wallpaper-switch-time', nightSwitchTime.toSettingsFormat());
+    }
+
+    _connectSettings() {
+        this._dayWallpaperImageChooserButton.connect('file-set', () => {
+            this._settings.set_string('day-wallpaper', this._dayWallpaperImageChooserButton.get_uri());
+        });
+        this._dayWallpaperAdjustmentComboBoxText.connect('changed', () => {
+            const activeItem = this._dayWallpaperAdjustmentComboBoxText.get_active();
+            this._settings.set_string('day-wallpaper-adjustment', this._pictureOptions[activeItem]);
+        });
+        this._dayWallpaperSwitchTimeWidget.hourSpinButton.connect('value-changed', this._onDayWallpaperSwitchTimeChanged.bind(this));
+        this._dayWallpaperSwitchTimeWidget.minuteSpinButton.connect('value-changed', this._onDayWallpaperSwitchTimeChanged.bind(this));
+
+        this._nightWallpaperImageChooserButton.connect('file-set', () => {
+            this._settings.set_string('night-wallpaper', this._nightWallpaperImageChooserButton.get_uri());
+        });
+        this._nightWallpaperAdjustmentComboBoxText.connect('changed', () => {
+            const activeItem = this._nightWallpaperAdjustmentComboBoxText.get_active();
+            this._settings.set_string('night-wallpaper-adjustment', this._pictureOptions[activeItem]);
+        });
+        this._nightWallpaperSwitchTimeWidget.hourSpinButton.connect('value-changed', this._onNightWallpaperSwitchTimeChanged.bind(this));
+        this._nightWallpaperSwitchTimeWidget.minuteSpinButton.connect('value-changed', this._onNightWallpaperSwitchTimeChanged.bind(this));
+    }
+
+    _capitalise(string) {
+        return string.charAt(0).toUpperCase() + string.slice(1)
+    }
+});
 
 function init() {
     Utils.checkExtensionSettings();
--- settingsUi.js
+++ settingsUi.js
@@ -4,189 +4,90 @@
 
 const { GObject, Gtk } = imports.gi;
 
-var WallpaperSettingsSection = GObject.registerClass(
-class WallpaperSettingsSection extends Gtk.Grid {
-    _init(title, pictureOptions) {
+var SwitchTimeWidget = GObject.registerClass(
+class SwitchTimeWidget extends Gtk.Box {
+    _init() {
         super._init({
-            column_spacing: 12,
-            row_spacing: 12,
+            spacing: 4,
+            halign: Gtk.Align.CENTER
         })
 
-        this._pictureOptions = pictureOptions.sort();
-
-        // Image files filter
-        const _imageFileFilter = new Gtk.FileFilter();
-        _imageFileFilter.set_name('Image files');
-        _imageFileFilter.add_mime_type('image/jpg');
-        _imageFileFilter.add_mime_type('image/png');
-        _imageFileFilter.add_mime_type('image/jpeg');
-        _imageFileFilter.add_mime_type('image/bmp');
-
-        // Title
-        const _sectionTitleLabel = new Gtk.Label({
-            label: `<b>${title}</b>`,
-            halign: Gtk.Align.START,
-            hexpand: true,
-            use_markup: true
+        const _hourSpinAdustment = new Gtk.Adjustment({
+            lower: 0,
+            upper: 23,
+            step_increment: 1
         });
-        this.attach(_sectionTitleLabel, 0, 1, 1, 1)
+        const _minuteSpinAdustment = new Gtk.Adjustment({
+            lower: 0,
+            upper: 59,
+            step_increment: 1
+        });
+        const _timeColonLabel = Gtk.Label.new(':');
 
-        // Image
-        const _imageLabel = new Gtk.Label({
-            label: 'Image',
-            halign: Gtk.Align.START,
-            hexpand: true
+        this.hourSpinButton = new Gtk.SpinButton({
+            adjustment: _hourSpinAdustment,
+            wrap: true,
+            width_chars: 2,
+            max_width_chars: 2,
+            snap_to_ticks: true,
+            numeric: true,
+            orientation: Gtk.Orientation.VERTICAL
         });
 
-        this.imageChooserButton = new Gtk.FileChooserButton({
-            title: 'Image File',
-            action: Gtk.FileChooserAction.OPEN,
-            halign: Gtk.Align.END,
-            width_chars: 40,
-            filter: _imageFileFilter
+        this.minuteSpinButton = new Gtk.SpinButton({
+            adjustment: _minuteSpinAdustment,
+            wrap: true,
+            width_chars: 2,
+            max_width_chars: 2,
+            snap_to_ticks: true,
+            numeric: true,
+            orientation: Gtk.Orientation.VERTICAL
         });
 
-        this.attach(_imageLabel, 0, 2, 1, 1);
-        this.attach_next_to(this.imageChooserButton, _imageLabel, Gtk.PositionType.RIGHT, 1, 1);
+        this.hourSpinButton.connect('output', this._padValueWithLeadingZero.bind(this));
+        this.minuteSpinButton.connect('output', this._padValueWithLeadingZero.bind(this));
 
-        // Adjustment
-        const _adjustmentLabel = new Gtk.Label({
-            label: 'Adjustment',
-            halign: Gtk.Align.START,
-            hexpand: true
-        });
-
-        this.adjustmentComboBoxText = new Gtk.ComboBoxText();
-        for (let i = 0; i < this._pictureOptions.length; i++) {
-            this.adjustmentComboBoxText.append_text(this._capitalise(this._pictureOptions[i]));
-        }
-
-        this.attach(_adjustmentLabel, 0, 3, 1, 1);
-        this.attach_next_to(this.adjustmentComboBoxText, _adjustmentLabel, Gtk.PositionType.RIGHT, 1, 1);
-
-        // Switch Time
-        const _switchTimeLabel = new Gtk.Label({
-            label: 'Switch Time (24-hour)',
-            halign: Gtk.Align.START,
-            hexpand: true
-        });
-
-        this.switchTimeWidget = new SwitchTimeWidget();
-
-        this.attach(_switchTimeLabel, 0, 4, 1, 1);
-        this.attach_next_to(this.switchTimeWidget, _switchTimeLabel, Gtk.PositionType.RIGHT, 1, 1);
+        this.pack_start(this.hourSpinButton, false, false, 0);
+        this.pack_start(_timeColonLabel, false, false, 0);
+        this.pack_start(this.minuteSpinButton, false, false, 0);
     }
 
-    setWallpaperUri(uri) {
-        this.imageChooserButton.set_uri(uri);
+    setSwitchTime(switchHour, switchMinute) {
+        this.hourSpinButton.set_value(switchHour);
+        this.minuteSpinButton.set_value(switchMinute);
     }
 
-    getWallpaperUri() {
-        return this.imageChooserButton.get_uri();
-    }
-
-    getWallpaperAdjustment() {
-        const activeItem = this.adjustmentComboBoxText.get_active();
-        return this._pictureOptions[activeItem];
-    }
-
-    setWallpaperAdjustment(adjustmentOption) {
-        this.adjustmentComboBoxText.set_active(this._pictureOptions.indexOf(adjustmentOption));
-    }
-
-    setWallpaperSwitchTime(switchHour, switchMinute) {
-        this.switchTimeWidget.setSwitchTime(switchHour, switchMinute);
-    }
-
-    _capitalise(string) {
-        return string.charAt(0).toUpperCase() + string.slice(1)
+    _padValueWithLeadingZero(spinButton) {
+        const adjustment = spinButton.get_adjustment();
+        const value = parseInt(adjustment.get_value());
+        const paddedValue = (value < 10 ? '0' : '') + value;
+        spinButton.set_text(paddedValue);
+        return true;
     }
 });
 
-var SwitchTimeWidget = GObject.registerClass(
-    class SwitchTimeWidget extends Gtk.Box {
-        _init() {
-            super._init({
-                spacing: 4,
-                halign: Gtk.Align.CENTER
-            })
-
-            const _hourSpinAdustment = new Gtk.Adjustment({
-                lower: 0,
-                upper: 23,
-                step_increment: 1
-            });
-            const _minuteSpinAdustment = new Gtk.Adjustment({
-                lower: 0,
-                upper: 59,
-                step_increment: 1
-            });
-            const _timeColonLabel = Gtk.Label.new(':');
-
-            this.hourSpinButton = new Gtk.SpinButton({
-                adjustment: _hourSpinAdustment,
-                wrap: true,
-                width_chars: 2,
-                max_width_chars: 2,
-                snap_to_ticks: true,
-                numeric: true,
-                orientation: Gtk.Orientation.VERTICAL
-            });
-
-            this.minuteSpinButton = new Gtk.SpinButton({
-                adjustment: _minuteSpinAdustment,
-                wrap: true,
-                width_chars: 2,
-                max_width_chars: 2,
-                snap_to_ticks: true,
-                numeric: true,
-                orientation: Gtk.Orientation.VERTICAL
-            });
-
-            this.hourSpinButton.connect('output', this._padValueWithLeadingZero.bind(this));
-            this.minuteSpinButton.connect('output', this._padValueWithLeadingZero.bind(this));
-
-            this.pack_start(this.hourSpinButton, false, false, 0);
-            this.pack_start(_timeColonLabel, false, false, 0);
-            this.pack_start(this.minuteSpinButton, false, false, 0);
-        }
-
-        setSwitchTime(switchHour, switchMinute) {
-            this.hourSpinButton.set_value(switchHour);
-            this.minuteSpinButton.set_value(switchMinute);
-        }
-
-        _padValueWithLeadingZero(spinButton) {
-            const adjustment = spinButton.get_adjustment();
-            const value = parseInt(adjustment.get_value());
-            const paddedValue = (value < 10 ? '0' : '') + value;
-            spinButton.set_text(paddedValue);
-            return true;
-        }
-    });
-
 var AboutSection = GObject.registerClass(
-    class AboutSection extends Gtk.Box {
-        _init() {
-            super._init({
-                orientation: Gtk.Orientation.VERTICAL,
-                spacing: 6,
-                halign: Gtk.Align.CENTER
-            })
+class AboutSection extends Gtk.Box {
+    _init() {
+        super._init({
+            orientation: Gtk.Orientation.VERTICAL,
+            spacing: 6,
+            halign: Gtk.Align.CENTER
+        })
 
-            const createdByLabel = new Gtk.Label({
-                label: '<span font="10" foreground="#6c757d">Created by Swapnil Madavi</span>',
-                halign: Gtk.Align.CENTER,
-                use_markup: true
-            });
+        const createdByLabel = new Gtk.Label({
+            label: '<span font="10" foreground="#6c757d">Created by Swapnil Madavi</span>',
+            halign: Gtk.Align.CENTER,
+            use_markup: true
+        });
 
-            const homepageLabel = new Gtk.Label({
-                label: '<a href="https://github.com/swapnilmadavi/day-night-wallpaper-gnome-extension"><span foreground="#6c757d">Homepage</span></a>',
-                halign: Gtk.Align.CENTER,
-                use_markup: true
-            });
+        const homepageLabel = new Gtk.Label({
+            label: '<a href="https://github.com/swapnilmadavi/day-night-wallpaper-gnome-extension"><span foreground="#6c757d">Homepage</span></a>',
+            halign: Gtk.Align.CENTER,
+            use_markup: true
+        });
 
-            this.pack_start(createdByLabel, false, true, 0);
-            this.pack_start(homepageLabel, false, true, 0);
-        }
-    });
+        this.pack_start(createdByLabel, false, true, 0);
+        this.pack_start(homepageLabel, false, true, 0);
+    }
+});
